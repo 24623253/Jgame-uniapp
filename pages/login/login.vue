2a6789b50113211052227b42828b27ac7bf47cac
liuhongwei <24623253@qq.com>
2021-10-12 16:38:42 +0800

    授权弹窗触发改为getUserProfile

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -335,43 +335,7 @@
 								}
 							})
 			            },
-			
-			getUserInfo(){
-				uni.getProvider({
-				                      service: 'oauth',
-				                      success: function (res) {
-				                        if (~res.provider.indexOf('weixin')) {
-				                            uni.login({
-				                                provider: 'weixin',
-				                                success: (res2) => {
-				                                    
-				                                    uni.getUserInfo({
-				                                        provider: 'weixin',
-				                                        success: (info) => {//这里请求接口
-				                                            console.log(res2);
-				                                            console.log(info);
-				                                            
-				                                        },
-				                                        fail: () => {
-				                                            uni.showToast({title:"微信登录授权失败",icon:"none"});
-				                                        }
-				                                    })
-				                            
-				                                },
-				                                fail: () => {
-				                                    uni.showToast({title:"微信登录授权失败",icon:"none"});
-				                                }
-				                            })
-				                            
-				                        }else{
-				                            uni.showToast({
-				                                title: '请先安装微信或升级版本',
-				                                icon:"none"
-				                            });
-				                        }
-				                      }
-				                    });
-			}
+		
 
 		}
 	}
